9192b1410786e6533845a000eba8aaf51de32261
brass <brass@HEEHEEHEEHAA>
2025-09-18 12:09:13 -0400

    Exercise 2 complete

--- TP1/exo2.s
+++ TP1/exo2.s
@@ -1,1 +1,38 @@
-a
+.text
+ld n
+add inc
+mul n
+shr
+st max
+ld n
+add curradr
+st adrmax
+loop:
+lda curradr
+ld result
+addx
+st result
+adda inc
+sta curradr
+suba adrmax
+sta n
+ld n
+brnz loop
+ld max
+sub result
+st result
+stop
+.data
+#nums
+a: 4
+b: 0
+c: 3
+d: 5
+e: 1
+
+n: 5 #elements
+max: 0
+curradr: 22
+adrmax: 0
+inc: 1
+result: 0
