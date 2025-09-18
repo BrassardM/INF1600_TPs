7f63ed85665e3bd3882eb46387808a0039ee586f
DavPoly <david.pretty@polymtl.ca>
2025-09-18 12:23:18 -0400

    exo 1

--- TP1/exo1.s
+++ TP1/exo1.s
@@ -1,1 +1,26 @@
-a
+# 47ffe27e-3765-4024-9c85-1fac1395893e
+.text
+ld n
+sub one
+st n
+boucle:
+ld first
+add second
+st somme
+ld second
+st first
+ld somme
+st second
+ld n
+sub one
+st n
+brnz boucle
+ld somme
+stop
+
+.data
+n: 5
+first: 0
+second: 1
+somme: 0
+one: 1
