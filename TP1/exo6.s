b9e527923e0a4990633a9a450c69fa672cb7d32d
DavPoly <david.pretty@polymtl.ca>
2025-09-18 14:53:04 -0400

    exo6 almost finished

--- TP1/exo6.s
+++ TP1/exo6.s
@@ -1,1 +1,39 @@
-a
+# a17c33fc-e19a-4732-9df8-b5c605d6bl53
+.text
+lda adr
+ld zero
+sti
+adda one
+ld one
+sti
+adda one
+sti
+
+ld n
+sub three
+st n
+
+loop:
+lda adr
+ld zero
+add two
+add three
+sti
+
+adda one
+sta adr
+
+ld n
+sub one
+st n
+
+brnz loop
+stop
+
+.data
+n: 4
+adr: 29 # <- À modifier
+zero: 0
+one: 1
+two: 2
+three: 3
