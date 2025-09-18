772b0ae1091c5b4c14b436c581cb72192b79b5ac
DavPoly <david.pretty@polymtl.ca>
2025-09-18 14:06:58 -0400

    exo4 finished

--- TP1/exo4.s
+++ TP1/exo4.s
@@ -1,1 +1,30 @@
-a
+# 19f1a60d-862a-4f3b-bc52-c286328d28b1
+.text
+boucle: 
+ld i
+add one
+st i
+
+lda adr
+add one
+mul i
+shr
+sti
+
+adda one
+sta adr
+
+ld n
+sub one
+st n
+brnz boucle
+
+ld adr
+sub i
+stop
+
+.data
+n: 4
+i: 0
+adr: 24 # <- Droit de modifier
+one: 1a
