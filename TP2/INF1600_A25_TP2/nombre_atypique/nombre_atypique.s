191fdd8ad9d0f0642dd79bcf37b209b8c1e9b402
brass <brass@HEEHEEHEEHAA>
2025-10-01 13:26:10 -0400

    nombre_atypique commit (it works well)

--- TP2/INF1600_A25_TP2/nombre_atypique/nombre_atypique.s
+++ TP2/INF1600_A25_TP2/nombre_atypique/nombre_atypique.s
@@ -22,6 +22,43 @@ mov     %esp,%ebp
 
 # Votre code ici (cette ligne peut etre enlevee)
 
+#check if table is odd or even
+checkfirst:
+movl $tableau, %edx
+movl (%edx), %eax
+andl $1, %eax
+movl %eax, %ebx
+addl $4, %edx
+checksecond:
+movl (%edx), %eax
+andl $1, %eax
+cmpl %eax, %ebx
+jne checkthird
+checkeq:
+jmp findinit
+checkthird:
+addl $4, %edx
+movl (%edx), %eax
+andl $1, %eax
+movl %eax, %ebx
+
+findinit:
+movl n, %ecx
+movl $tableau, %edx
+findloop:
+movl (%edx), %eax
+andl $1, %eax
+cmpl %eax, %ebx
+jne print
+addl $4, %edx
+loop findloop
+
+
+print:
+pushl (%edx)
+pushl $format_single_number
+call printf
+
 # epilogue (cours 5)        # (ne pas modifier les 3 prochaines lignes)
 movl $0, %eax               # valeur de retour (0 = OK)
 leave
