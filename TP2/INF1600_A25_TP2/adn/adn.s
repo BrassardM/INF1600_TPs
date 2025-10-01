74682948c43797ed525c08be5f84b668a31a8987
brass <brass@HEEHEEHEEHAA>
2025-10-01 12:13:29 -0400

    adn commit (it works well)

--- TP2/INF1600_A25_TP2/adn/adn.s
+++ TP2/INF1600_A25_TP2/adn/adn.s
@@ -15,6 +15,47 @@ push    %ebp
 mov     %esp,%ebp
 
 # Votre code ici (cette ligne peut etre enlevee)
+movl $dna,%edi 
+
+# Iterer sur chaque lettre
+charloop:
+movb (%edi), %al
+cmpb $0, %al
+je printseq
+cmpb $'A', %al
+je ifa
+cmpb $'T', %al
+je ift
+cmpb $'C', %al
+je ifc
+cmpb $'G', %al
+je ifg
+jmp end
+
+ifa:
+movb $'T', %al
+jmp endofifs 
+
+ift:
+movb $'A', %al
+jmp endofifs
+
+ifc:
+movb $'G', %al
+jmp endofifs
+ifg:
+movb $'C', %al
+
+endofifs:
+movb %al, (%edi)
+incl %edi
+jmp charloop
+
+
+printseq:
+pushl $dna
+call printf
+end:
 
 # epilogue (cours 5)        # (ne pas modifier les 3 prochaines lignes)
 movl $0, %eax               # valeur de retour (0 = OK)
